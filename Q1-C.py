f7ee080124f8ea904255f650d0ea792ab71c86b2
zeinabzahiismael <105242654+zeinabzahiismael@users.noreply.github.com>
2022-05-11 17:45:35 +0300

    Add files via upload

---
+++ Q1-C.py
@@ -0,0 +1,20 @@
+# طريقة1
+l=['Network','Math','Programming','Physics','Music']
+for i in range(len(l)):
+    if l[i][0]=='P':
+        print(l[i])
+
+# طريقة2
+for i in range(len(l)):
+    if 'P' in l[i][0]:
+        print(l[i])
+
+
+
+
+
+
+
+
+
+
